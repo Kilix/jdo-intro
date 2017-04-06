dc9b2a05d9715f1aa9006145aaf53ca465e675a9
William <wcastand@gmail.com>
2017-04-06 16:29:02 +0200

    Track 1 files into repository.
    - modified components/avatar.js
    
    Auto commit by GitBook Editor

--- components/avatar.js
+++ components/avatar.js
@@ -9,7 +9,7 @@ const avatar = ({ src }) => ({
   height: 200,
   border: '2px solid #FFF',
   borderRadius: '50%',
-  boxShadow: '2px 2px 2px 2px rgba(31, 31, 31, 1)',
+  boxShadow: '2px 2px 1px 2px rgba(31, 31, 31, 1)',
 });
 
 export default createComponent(avatar, 'div');
